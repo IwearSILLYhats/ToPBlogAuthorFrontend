8b9b0f9336465ad7fac8f64ead786556648a3c5c
Austin <austin.moorer@gmail.com>
2025-05-03 18:43:29 -0400

    Installed tiptap and got the simple template working

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import Tiptap from "./components/Tiptap";
+
+function App() {
+  return (
+    <div className="card">
+      <Tiptap />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Tiptap.jsx
@@ -0,0 +1,7 @@
+import { SimpleEditor } from "./tiptap-templates/simple/simple-editor";
+
+function Tiptap() {
+  return <SimpleEditor />;
+}
+
+export default Tiptap;
